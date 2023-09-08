827bd4395cf899a25c5af362a4fcfbbba626d675
Mahmoud Taha <mahmoud.adeltaha1@vodafone.com>
2023-09-08 18:21:53 +0200

    Added DTOs

--- app/src/main/java/com/example/movies/movie/detail/data/source/remote/model/dto/MovieDetailsDto.kt
+++ app/src/main/java/com/example/movies/movie/detail/data/source/remote/model/dto/MovieDetailsDto.kt
@@ -1,4 +1,34 @@
 package com.example.movies.movie.detail.data.source.remote.model.dto
 
-class MovieDetailsDto {
-}
+import com.google.gson.annotations.SerializedName
+
+data class MovieDetailsDto(
+    @SerializedName("id")
+    val id: Long,
+
+    @SerializedName("title")
+    val title: String,
+
+    @SerializedName("release_date")
+    val releaseDate: String,
+
+    @SerializedName("poster_path")
+    val posterUrl: String,
+
+    @SerializedName("overview")
+    val overview: String,
+
+    @SerializedName("vote_average")
+    val rating: Double,
+
+    @SerializedName("genres")
+    val genres: List<Genre>,
+)
+
+data class Genre(
+    @SerializedName("id")
+    val id: Int,
+
+    @SerializedName("name")
+    val name: String,
+)
--- app/src/main/java/com/example/movies/movie/list/data/source/remote/model/dto/MoviesDto.kt
+++ app/src/main/java/com/example/movies/movie/list/data/source/remote/model/dto/MoviesDto.kt
@@ -1,4 +1,31 @@
 package com.example.movies.movie.list.data.source.remote.model.dto
 
-class MoviesDto {
-}
+import com.google.gson.annotations.SerializedName
+
+data class MoviesDto(
+    @SerializedName("page")
+    val page: Int? = null,
+
+    @SerializedName("total_pages")
+    val totalPages: Int? = null,
+
+    @SerializedName("total_results")
+    val totalResults: Long? = null,
+
+    @SerializedName("results")
+    val results: List<Movie> = emptyList(),
+)
+
+data class Movie(
+    @SerializedName("id")
+    val id: Long,
+
+    @SerializedName("title")
+    val title: String,
+
+    @SerializedName("release_date")
+    val releaseDate: String,
+
+    @SerializedName("poster_path")
+    val posterUrl: String
+)
