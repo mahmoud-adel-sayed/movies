bbcbb55551f006fb5f158a236f331d2ab4090f7d
Mahmoud Taha <mahmoud.adeltaha1@vodafone.com>
2023-09-08 18:40:35 +0200

    Added entities

--- app/src/main/java/com/example/movies/movie/list/data/source/local/dao/MoviesDao.kt
+++ app/src/main/java/com/example/movies/movie/list/data/source/local/dao/MoviesDao.kt
@@ -1,7 +1,16 @@
 package com.example.movies.movie.list.data.source.local.dao
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.movies.movie.list.data.source.local.entity.MovieEntity
 
 @Dao
 interface MoviesDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(movieEntity: MovieEntity)
+
+    @Query("SELECT * from movies")
+    suspend fun getAllMovies(): List<MovieEntity>
 }
--- app/src/main/java/com/example/movies/movie/list/data/source/local/entity/MovieEntity.kt
+++ app/src/main/java/com/example/movies/movie/list/data/source/local/entity/MovieEntity.kt
@@ -1,7 +1,21 @@
 package com.example.movies.movie.list.data.source.local.entity
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity(tableName = "movies")
-class MovieEntity {
-}
+data class MovieEntity(
+    @PrimaryKey
+    @ColumnInfo(name = "id")
+    var id: Int,
+
+    @ColumnInfo(name = "title")
+    var title: String,
+
+    @ColumnInfo(name = "release_date")
+    var releaseDate: String,
+
+    @ColumnInfo(name = "poster_url")
+    var posterUrl: String
+)
